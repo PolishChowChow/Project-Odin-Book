3b04ecba6085d5dc401eb63c5ef7e260f56c33fb
PolishChowChow <bmaselko98@gmail.com>
2024-10-23 19:59:00 +0200

    feat: impltemented show password button

--- client/src/components/Controls/FormInput.tsx
+++ client/src/components/Controls/FormInput.tsx
@@ -1,17 +1,31 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useState } from "react";
 import TextErrorElement from "../Error/TextErrorElement";
 type FormInputProps = {
   // errorMessage: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export default function FormInput({ id, ...props }: FormInputProps) {
+export default function FormInput({ id, type, ...props }: FormInputProps) {
+  const [isPasswordType, setIsPasswordType] = useState(true);
+  const toggleType = () => {
+    setIsPasswordType((prevPasswordType) => !prevPasswordType);
+  };
+
   return (
     <>
-      <label htmlFor={id}>
+      <label htmlFor={id} className="relative">
         <input
+          type={`${isPasswordType ? type : "text"}`}
           className="text-sm border border-gray-400 p-2 rounded outline-none focus:border-blue-900 transition-colors focus:placeholder:text-stone-800 w-full"
           {...props}
         />
+        <button
+          onClick={toggleType}
+          className={`${
+            type === "password" ? "block" : "hidden"
+          } absolute top-2 right-2 text-sm text-blue-600 hover:text-blue-900`}
+        >
+          {isPasswordType ? "show" : "hide"}
+        </button>
         <TextErrorElement message="" />
       </label>
     </>
