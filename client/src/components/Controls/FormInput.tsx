c4777f80c3562bfd1dddbfdee4babdeb694baf50
PolishChowChow <bmaselko98@gmail.com>
2024-10-23 20:08:03 +0200

    feat: implemented storing whole form register data in state

--- client/src/components/Controls/FormInput.tsx
+++ client/src/components/Controls/FormInput.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, useState } from "react";
+import { InputHTMLAttributes, useEffect, useState } from "react";
 import TextErrorElement from "../Error/TextErrorElement";
 type FormInputProps = {
   // errorMessage: string;
@@ -9,12 +9,16 @@ export default function FormInput({ id, type, ...props }: FormInputProps) {
   const toggleType = () => {
     setIsPasswordType((prevPasswordType) => !prevPasswordType);
   };
+  useEffect(()=>{
+    console.log(props.value)
+  },[props.value])
 
   return (
     <>
       <label htmlFor={id} className="relative">
         <input
           type={`${isPasswordType ? type : "text"}`}
+          id={id}
           className="text-sm border border-gray-400 p-2 rounded outline-none focus:border-blue-900 transition-colors focus:placeholder:text-stone-800 w-full"
           {...props}
         />
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,12 +1,41 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import Button from "../components/Controls/Button";
 import FormInput from "../components/Controls/FormInput";
 import CustomLink from "../components/Controls/CustomLink";
 
+type RegisterDataType = {
+    email: string;
+    username: string;
+    password: string;    
+}
+
+type RegisterFormDataType = {
+    confirmPassword: string;
+} & RegisterDataType
+
+const initialDataType:RegisterFormDataType = {
+    email: "",
+    username: "",
+    password: "",    
+    confirmPassword: ""    
+}
 export default function RegisterPage() {
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
+
+  const [formData, setFormData] = useState<RegisterFormDataType>(initialDataType);
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    console.log(id, value)
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [id]: value
+        }
+    })
+  }
+
   return (
     <div className="h-lvh flex flex-col">
       <header>
@@ -19,10 +48,10 @@ export default function RegisterPage() {
         >
           <h2 className="text-3xl mt-5">Register</h2>
           <p className="text-sm">Join our awesome community</p>
-          <FormInput placeholder="Email address" type="email" />
-          <FormInput placeholder="Username" type="text" />
-          <FormInput type="password" placeholder="Password" />
-          <FormInput type="password" placeholder="Confirm Password" />
+          <FormInput placeholder="Email address" type="email" id="email" name="email" onChange={handleChange} value={formData.email}/>
+          <FormInput placeholder="Username" type="text" id="username" name="username"  onChange={handleChange} value={formData.username}/>
+          <FormInput type="password" placeholder="Password" id="password" name="confirmPassword"  onChange={handleChange} value={formData.password}/>
+          <FormInput type="password" placeholder="Confirm Password" id="confirmPassword" name="confirmPassword" onChange={handleChange} value={formData.confirmPassword} />
           <Button type="submit">Submit</Button>
         </form>
         <p className="mt-5">
